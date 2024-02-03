e81bff07cd5609f9c4636f21e231da2b00d451c5
Shourya Tyagi <78632193+Shouryatyagi@users.noreply.github.com>
2024-02-03 22:22:49 +0530

    Update even_odd.cpp

--- Gettinng started/even_odd.cpp
+++ Gettinng started/even_odd.cpp
@@ -1,7 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 void even_odd(int n){
+    // bruteforce approach
     (n%2==0)? cout<<"Even": cout<<"Odd";
+
+    // bit manipulation
+    // (n&1)?cout<<"Odd":cout<<"Even";
 }
 int main()
 {
